118fc7dec3f18a49fa9cbee9396c929e95214f77
fernando Urbano <fernando_urbano@outlook.com>
2025-08-24 00:42:01 -0300

    ejercicio 05

---
+++ js/06-POO.js
@@ -0,0 +1,10 @@
+// Libros - Dificultad:  🟢🟡🔴
+// 6- Crear una clase Libro que contenga al menos las siguientes propiedades:
+// ISBN
+// Título
+// Autor
+// Número de páginas
+
+// Crear sus respectivos métodos get y set correspondientes para cada propiedad. Crear el método mostrarLibro() para mostrar la información relativa al libro con el siguiente formato:
+
+// “El libro xxx con ISBN xxx creado por el autor xxx tiene páginas xxx”
